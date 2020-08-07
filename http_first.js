17f55ee3a3aeb6a06f932f0639f52a08eff37ba4
jhys <1770613507@qq.com>
2020-08-07 21:07:32 +0800

    update

---
+++ http_first.js
@@ -0,0 +1,11 @@
+
+const http = require("http");
+const myModule = require('./node/myModule')
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.end('Hello World\n');
+}).listen(8888);
+
+
+console.log('Server running at http://127.0.0.1:8888/');
---
+++ test_export.js
@@ -0,0 +1,12 @@
+
+const arrFn = require('./exports')
+
+const arr = [1,2,3,3,2];
+
+let  noRepeat = arrFn.noRepeat(arr);
+
+let num = arrFn.add(arr);
+
+console.log(noRepeat);
+
+console.log(num);
